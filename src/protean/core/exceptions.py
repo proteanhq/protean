bd832db23645e618015c1d3cd4e9a51b426386d4
abhishekram <abhishek8816@gmail.com>
2018-09-26 11:59:37 +0530

    setup the abstract Field class

--- src/protean/core/exceptions.py
+++ src/protean/core/exceptions.py
@@ -9,3 +9,37 @@ class ImproperlyConfigured(Exception):
 
 class ObjectNotFoundException(Exception):
     """Object was not found, can raise 404"""
+
+
+class ValidationError(Exception):
+    """Raised when validation fails on a field. Validators and custom fields should
+    raise this exception.
+
+    :param message: An error message
+    :param list field_names: Field names to store the error on. If `None`, the
+     error is stored in its default location.
+    """
+
+    def __init__(self, message, field_names=None):
+        # String, list, or dictionary of error messages.
+        if not isinstance(message, dict) and not isinstance(message, list):
+            messages = [message]
+        else:
+            messages = message
+        self.messages = messages
+
+        # List of field_names which failed validation.
+        if isinstance(field_names, str):
+            self.field_names = [field_names]
+        else:
+            self.field_names = field_names or []
+
+    @property
+    def normalized_messages(self, no_field_name='_entity'):
+        """Return all the error messages as a dictionary"""
+        if isinstance(self.messages, dict):
+            return self.messages
+        if len(self.field_names) == 0:
+            return {no_field_name: self.messages}
+
+        return dict((name, self.messages) for name in self.field_names)
