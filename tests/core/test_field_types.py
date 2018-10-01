168037a8ec98a5b59cab8eb8f60e06354547d84e
Abhishek Ram <abhishek.ram@me.com>
2018-10-01 15:44:31 +0530

    add the integer Field with test cases (#14)
    Add the integer Field with test cases. Also:
    * disable bandit for tests

--- tests/core/test_field_types.py
+++ tests/core/test_field_types.py
@@ -1,6 +1,9 @@
 """ Test cases for all available field type implementations"""
 
-import pytest  # pylint: disable=E0401
+from decimal import Decimal
+
+import pytest
+
 from protean.core import field
 from protean.core.exceptions import ValidationError
 
@@ -33,3 +36,43 @@ class TestStringField:
         with pytest.raises(ValidationError):
             name = field.String(max_length=5)
             name.validate('Dummy Dummy')
+
+
+class TestIntegerField:
+    """ Test the Integer Field Implementation"""
+
+    def test_init(self):
+        """Test successful String Field initialization"""
+
+        age = field.Integer()
+        assert age is not None
+
+    def test_type_validation(self):
+        """ Test type checking validation for the Field"""
+        with pytest.raises(ValidationError):
+            age = field.Integer()
+            age.validate('x')
+
+    def test_min_value(self):
+        """ Test minimum value validation for the integer field"""
+
+        with pytest.raises(ValidationError):
+            age = field.Integer(min_value=5)
+            age.validate(3)
+
+    def test_max_value(self):
+        """ Test maximum value validation for the string field"""
+
+        with pytest.raises(ValidationError):
+            age = field.Integer(max_value=5)
+            age.validate(6)
+
+    def test_float_input(self):
+        """ Test that floats and decimals are converted to integer"""
+
+        age = field.Integer()
+        age.validate(3.1)
+        assert age.value == 3
+
+        age.validate(Decimal(3.1))
+        assert age.value == 3
