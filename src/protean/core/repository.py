b9be27fbea1f02c169087e29d7a3724b70620500
abhishekram <abhishek8816@gmail.com>
2018-10-22 14:47:49 +0530

    refactor repository to include config

--- src/protean/core/repository.py
+++ src/protean/core/repository.py
@@ -1,6 +1,7 @@
 """Abstract Repository Classes"""
 
 import logging
+import importlib
 
 from abc import ABCMeta
 from abc import abstractmethod
@@ -16,6 +17,7 @@ from protean.core.entity import Entity
 from protean.core.exceptions import ConfigurationError, \
     ObjectNotFoundError
 from protean.utils import OptionsMeta
+from protean.conf import active_config
 
 logger = logging.getLogger('protean.repository')
 
@@ -64,13 +66,13 @@ class Pagination(object):
             return None
 
 
-class Repository(metaclass=ABCMeta):
+class BaseRepository(metaclass=ABCMeta):
     """Repository interface to interact with databases"""
 
-    def __init__(self, db, schema):
-        self.db = db
-        self.schema = schema
-        self.schema_name = schema.__class__.__name__
+    def __init__(self, conn, schema_cls):
+        self.conn = conn
+        self.schema = schema_cls()
+        self.schema_name = schema_cls.__name__
 
     @abstractmethod
     def _create(self, entity: Entity):
@@ -181,7 +183,7 @@ class Repository(metaclass=ABCMeta):
 class RepositorySchemaOpts(object):
     """class Meta options for the :class:`RepositorySchema`."""
 
-    def __init__(self, meta, schema):
+    def __init__(self, meta, schema_cls):
         self.entity = getattr(meta, 'entity', None)
         if not self.entity or not issubclass(self.entity, Entity):
             raise ConfigurationError(
@@ -190,7 +192,10 @@ class RepositorySchemaOpts(object):
         # Get the schema name to be used, if not provided default it
         self.schema_name = getattr(meta, 'schema_name', None)
         if not self.schema_name:
-            self.schema_name = inflection.underscore(schema.__name__)
+            self.schema_name = inflection.underscore(schema_cls.__name__)
+
+        # Get the database bound to this schema
+        self.bind = getattr(meta, 'bind', 'default')
 
 
 class RepositorySchema(metaclass=OptionsMeta):
@@ -212,40 +217,79 @@ class RepositorySchema(metaclass=OptionsMeta):
         """Convert Repository Schema Object to Entity Object"""
 
 
-class RepositoryFactory(metaclass=ABCMeta):
+class RepositoryFactory:
     """Repository Factory interface to retrieve resource repositories"""
 
     def __init__(self):
         """"Initialize repository factory"""
         self._registry = {}
-        self._connection = None
+        self._connections = {}
+        self._repositories = None
 
-    @abstractmethod
-    def get_connection(self):
-        """Initialize the repository and return the database object"""
+    @property
+    def repositories(self):
+        """ Return the databases configured for the application"""
+        if self._repositories is None:
+            self._repositories = active_config.REPOSITORIES
 
-    def register(self, repo: Repository, schema: RepositorySchema):
-        """ Register given schema against a repository
-        :param repo: The repository to be used for a schema
-        :param schema: The schema to be mapped to a repository
+        if not isinstance(self._repositories, dict) or self._repositories == {}:
+            raise ConfigurationError(
+                "'REPOSITORIES' config must be a dict and at least one "
+                "database must be defined")
+
+        if 'default' not in self._repositories:
+            raise ConfigurationError(
+                "You must define a 'default' repository")
+
+        return self._repositories
+
+    def register(self, schema_cls, repo_cls=None):
+        """ Register the given schema with the factory
+        :param schema_cls: class of the schema to be registered
+        :param repo_cls: Optional repository class to use if not the
+        `Repository` defined by the provider is userd
         """
-        if not issubclass(schema, RepositorySchema):
+        if not issubclass(schema_cls, RepositorySchema):
             raise AssertionError(
-                f'Schema {schema} must be subclass of `RepositorySchema`')
+                f'Schema {schema_cls} must be subclass of `RepositorySchema`')
 
-        if not issubclass(repo, Repository):
+        if repo_cls and not issubclass(repo_cls, BaseRepository):
             raise AssertionError(
-                f'Repo {repo} must be subclass of `Repository`')
+                f'Repository {repo_cls} must be subclass of `BaseRepository`')
 
-        if schema not in self._registry:
-            db = self.get_connection()
-            self._registry[schema.__name__] = repo(db, schema())
+        # Register the schema if it does not exist
+        schema_name = schema_cls.__name__
+        if schema_name not in self._registry:
+            # Lookup the connection details for the schema
+            try:
+                conn_info = self.repositories[schema_cls.opts.bind]
+            except KeyError:
+                raise ConfigurationError(
+                    f"'{schema_cls.opts.bind}' repository not found in "
+                    f"'REPOSITORIES'")
+
+            # Load the repository provider
+            provider = importlib.import_module(conn_info['PROVIDER'])
+
+            # If no connection exists then build it
+            if schema_cls.opts.bind not in self._connections:
+                conn_handler = provider.ConnectionHandler(conn_info)
+                self._connections[schema_cls.opts.bind] = \
+                    conn_handler.get_connection()
+
+            # Finally register the schema against the provider repository
+            repo_cls = repo_cls or provider.Repository
+            self._registry[schema_name] = repo_cls(
+                self._connections[schema_cls.opts.bind], schema_cls)
             logger.debug(
-                f'Registered schema {schema.__name__} with repository '
-                f'{repo.__name__}.')
+                f'Registered schema {schema_name} with repository provider '
+                f'{conn_info["PROVIDER"]}.')
 
     def __getattr__(self, schema):
         try:
             return self._registry[schema]
         except KeyError:
             raise AssertionError('Unregistered Schema')
+
+
+rf = RepositoryFactory()
--- tests/core/test_repository.py
+++ tests/core/test_repository.py
@@ -3,10 +3,11 @@
 import pytest
 
 from protean.core.entity import Entity
+from protean.core.repository import rf
 from protean.core.exceptions import ValidationError, ObjectNotFoundError
 from protean.core import field
 
-from ..support.dict_repo import drf, DictSchema, DictRepository
+from ..support.dict_repo import DictSchema
 
 
 class Dog(Entity):
@@ -26,7 +27,7 @@ class DogSchema(DictSchema):
         schema_name = 'dogs'
 
 
-drf.register(DictRepository, DogSchema)
+rf.register(DogSchema)
 
 
 class TestRepository:
@@ -35,23 +36,23 @@ class TestRepository:
     def test_init(self):
         """Test successful access to the Dog repository"""
 
-        drf.DogSchema.filter()
-        current_db = dict(drf.DogSchema.db)
+        rf.DogSchema.filter()
+        current_db = dict(rf.DogSchema.conn)
         assert current_db == {'dogs': {}}
 
     def test_create(self):
         """ Add an entity to the repository"""
         with pytest.raises(ValidationError):
-            drf.DogSchema.create(name='Johnny', owner='John')
+            rf.DogSchema.create(name='Johnny', owner='John')
 
-        dog = drf.DogSchema.create(id=1, name='Johnny', owner='John')
+        dog = rf.DogSchema.create(id=1, name='Johnny', owner='John')
         assert dog is not None
         assert dog.id == 1
         assert dog.name == 'Johnny'
         assert dog.age == 5
         assert dog.owner == 'John'
 
-        dog = drf.DogSchema.get(1)
+        dog = rf.DogSchema.get(1)
         assert dog is not None
 
     def test_update(self):
@@ -59,39 +60,39 @@ class TestRepository:
 
         # Using an invalid id should fail
         with pytest.raises(ObjectNotFoundError):
-            drf.DogSchema.update(identifier=2, data=dict(age=10))
+            rf.DogSchema.update(identifier=2, data=dict(age=10))
 
         # Updates should run the validations
         with pytest.raises(ValidationError):
-            drf.DogSchema.update(identifier=1, data=dict(age='x'))
+            rf.DogSchema.update(identifier=1, data=dict(age='x'))
 
-        drf.DogSchema.update(identifier=1, data=dict(age=10))
-        u_dog = drf.DogSchema.get(1)
+        rf.DogSchema.update(identifier=1, data=dict(age=10))
+        u_dog = rf.DogSchema.get(1)
         assert u_dog is not None
         assert u_dog.age == 10
 
     def test_filter(self):
         """ Query the repository using filters """
         # Add multiple entries to the DB
-        drf.DogSchema.create(id=2, name='Murdock', age=7, owner='John')
-        drf.DogSchema.create(id=3, name='Jean', age=3, owner='John')
-        drf.DogSchema.create(id=4, name='Bart', age=6, owner='Carrie')
+        rf.DogSchema.create(id=2, name='Murdock', age=7, owner='John')
+        rf.DogSchema.create(id=3, name='Jean', age=3, owner='John')
+        rf.DogSchema.create(id=4, name='Bart', age=6, owner='Carrie')
 
         # Filter by the Owner
-        dogs = drf.DogSchema.filter(owner='John')
+        dogs = rf.DogSchema.filter(owner='John')
         assert dogs is not None
         assert dogs.total == 3
         assert len(dogs.items) == 3
 
         # Order the results by age
-        dogs = drf.DogSchema.filter(owner='John', order_by=['-age'])
+        dogs = rf.DogSchema.filter(owner='John', order_by=['-age'])
         assert dogs is not None
         assert dogs.first.age == 10
         assert dogs.first.name == 'Johnny'
 
     def test_pagination(self):
         """ Test the pagination of the filter results"""
-        dogs = drf.DogSchema.filter(per_page=2, order_by=['id'])
+        dogs = rf.DogSchema.filter(per_page=2, order_by=['id'])
         assert dogs is not None
         assert dogs.total == 4
         assert len(dogs.items) == 2
@@ -99,7 +100,7 @@ class TestRepository:
         assert dogs.has_next
         assert not dogs.has_prev
 
-        dogs = drf.DogSchema.filter(page=2, per_page=2, order_by=['id'])
+        dogs = rf.DogSchema.filter(page=2, per_page=2, order_by=['id'])
         assert len(dogs.items) == 2
         assert dogs.first.id == 3
         assert not dogs.has_next
@@ -107,11 +108,11 @@ class TestRepository:
 
     def test_delete(self):
         """ Delete an object in the reposoitory by ID"""
-        del_count = drf.DogSchema.delete(1)
+        del_count = rf.DogSchema.delete(1)
         assert del_count == 1
 
-        del_count = drf.DogSchema.delete(1)
+        del_count = rf.DogSchema.delete(1)
         assert del_count == 0
 
         with pytest.raises(ObjectNotFoundError):
-            drf.DogSchema.get(1)
+            rf.DogSchema.get(1)
