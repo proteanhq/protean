405e818dedd3d63d0e03f04eb9d120894bfbc046
abhishekram <abhishek8816@gmail.com>
2018-11-05 14:29:33 +0530

    make utils as a package and cleanup requirements

--- setup.py
+++ setup.py
@@ -64,8 +64,7 @@ setup(
     install_requires=[
         'click==6.7',
         'bleach==2.1.3',
-        'werkzeug==0.14.1',
-        'inflection==0.3.1'
+        'werkzeug==0.14.1'
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
---
+++ src/protean/utils/log.py
@@ -0,0 +1,12 @@
+"""Module defines logging related utilities """
+
+import logging.config
+
+from protean.conf import active_config
+
+
+def configure_logging():
+    """ Function to configure the logging for the Protean App"""
+    # Load the logger using the logging config
+    logging.config.dictConfig(
+        active_config.LOGGING_CONFIG)  # pylint: disable=E1101
