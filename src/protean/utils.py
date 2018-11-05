f9ea97c7187fc5470abfa2b6fc9986186cb2a657
abhishekram <abhishek8816@gmail.com>
2018-11-05 12:49:59 +0530

    add perform import utility and test cases

--- src/protean/utils.py
+++ src/protean/utils.py
@@ -4,6 +4,8 @@ from abc import ABCMeta
 
 import logging.config
 
+from importlib import import_module
+
 from protean.conf import active_config
 
 
@@ -31,3 +33,30 @@ def configure_logging():
     # Load the logger using the logging config
     logging.config.dictConfig(
         active_config.LOGGING_CONFIG)  # pylint: disable=E1101
+
+
+def perform_import(val):
+    """
+    If the given setting is a string import notation,
+    then perform the necessary import or imports.
+    """
+    if val is None:
+        return None
+    elif isinstance(val, str):
+        return import_from_string(val)
+    elif isinstance(val, (list, tuple)):
+        return [import_from_string(item) for item in val]
+    return val
+
+
+def import_from_string(val):
+    """
+    Attempt to import a class from a string representation.
+    """
+    try:
+        module_path, class_name = val.rsplit('.', 1)
+        module = import_module(module_path)
+        return getattr(module, class_name)
+    except (ImportError, AttributeError) as e:
+        msg = f"Could not import {val}. {e.__class__.__name__}: {e}"
+        raise ImportError(msg)
---
+++ tests/test_other.py
@@ -0,0 +1,31 @@
+"""Module to test other Protean Functions and Utilities"""
+
+import pytest
+
+from protean.utils import perform_import
+from protean.core import entity
+
+
+def test_perform_import():
+    """ Test the perform import function """
+
+    # Test importing of None
+    mod = perform_import(None)
+    assert mod is None
+
+    # Test import of string
+    mod = perform_import('protean.core.entity')
+    assert mod == entity
+
+    # Test import list
+    mod = perform_import(['protean.core.entity.Entity',
+                          'protean.core.entity.EntityBase'])
+    assert mod == [entity.Entity, entity.EntityBase]
+
+    # Test Failed import
+    with pytest.raises(ImportError):
+        perform_import('protean.core.entity.xxxx')
+
+    # Test Direct import
+    mod = perform_import(entity)
+    assert mod == entity
