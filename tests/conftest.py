84e95f996cee0f320ddd75f2e20775185930d159
abhishekram <abhishek8816@gmail.com>
2018-11-29 09:42:53 +0530

    make setting the PROTEAN_CONFIG mandatory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,4 @@
 """Module to setup Factories and other required artifacts for tests"""
-
 import os
 
-import mock
-import pytest
-
-
 os.environ['PROTEAN_CONFIG'] = 'tests.support.sample_config'
-
-
-@pytest.fixture(scope='module', autouse=True)
-@mock.patch.dict(os.environ, {'PROTEAN_CONFIG': 'tests.support.sample_config'})
-def config():
-    """Global Config fixture for all tests"""
-
-    from protean.conf import active_config
-    return active_config
