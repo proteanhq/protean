f8792c4d873076ce6a3e02873bebb11205d9965b
Subhash Bhushan <subhash.bhushan@gmail.com>
2019-02-22 14:42:36 -0800

    75 Referenced Objects - Interim Commit 1 - Implement Entity Cache

--- src/protean/core/entity.py
+++ src/protean/core/entity.py
@@ -356,6 +356,25 @@ class QuerySet:
         return self.all().has_prev
 
 
+class EntityStateFieldsCacheDescriptor:
+    def __get__(self, instance, cls=None):
+        if instance is None:
+            return self
+        res = instance.fields_cache = {}
+        return res
+
+
+class EntityState:
+    """Store entity instance state."""
+
+    # If true, uniqueness validation checks will consider this a new, unsaved
+    # object. Necessary for correct validation of new instances of objects with
+    # explicit (non-auto) PKs. This impacts validation only; it has no effect
+    # on the actual save.
+    adding = True
+    fields_cache = EntityStateFieldsCacheDescriptor()
+
+
 class Entity(metaclass=EntityBase):
     """The Base class for Protean-Compliant Domain Entities.
 
@@ -397,6 +416,9 @@ class Entity(metaclass=EntityBase):
         """
 
         self.errors = {}
+
+        # Set up the storage for instance state
+        self._state = EntityState()
 
         # Load the attributes based on the template
         loaded_fields = []
