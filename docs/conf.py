66dc276bc16edb01d8968f93de25618fd0fe5f81
Subhash Bhushan <subhash.bhushan@gmail.com>
2019-04-05 20:48:06 -0700

    Bump version: 0.0.9 → 0.0.10

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'Protean'
 year = '2019'
 author = 'Subhash Bhushan C'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.9'
+version = release = '0.0.10'
 
 pygments_style = 'autumn'
 templates_path = ['.']
--- src/protean/__init__.py
+++ src/protean/__init__.py
@@ -1,3 +1,3 @@
 """Primary Module to define version and expose packages"""
 
-__version__ = '0.0.9'
+__version__ = '0.0.10'
