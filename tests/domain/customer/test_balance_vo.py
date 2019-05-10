d8d8256622bdc8eafe28ec805ef5df1b5e574c4b
Subhash Bhushan <subhash.bhushan@gmail.com>
2019-05-10 13:36:14 -0700

    Additional test cases with Account Aggregate and Balance Value Object

---
+++ tests/domain/customer/test_balance_vo.py
@@ -0,0 +1,31 @@
+"""Test VO functionality with Balance Value Object"""
+
+# Protean
+from tests.support.sample_domain.customer.domain.model.account import Balance, Currency
+
+
+class TestBalanceVO:
+    def test_init(self):
+        """Test that direct initialization works"""
+        balance = Balance(currency=Currency.CAD.value, amount=0.0)
+        assert balance is not None
+        assert balance.currency == 'CAD'
+        assert balance.amount == 0.0
+
+    def test_init_build(self):
+        """Test that Balance VO can be initialized successfully"""
+        balance = Balance.build(currency=Currency.CAD.value, amount=0.0)
+        assert balance is not None
+        assert balance.currency == 'CAD'
+        assert balance.amount == 0.0
+
+    def test_equivalence(self):
+        """Test that two Balance VOs are equal if their values are equal"""
+        balance1 = Balance.build(currency=Currency.USD.value, amount=0.0)
+        balance2 = Balance.build(currency=Currency.USD.value, amount=0.0)
+
+        assert balance1 == balance2
+
+        balance3 = Balance.build(currency=Currency.INR.value, amount=0.0)
+
+        assert balance3 != balance1
---
+++ tests/support/sample_domain/customer/domain/model/account.py
@@ -0,0 +1,53 @@
+"""Account Aggregate"""
+
+from enum import Enum
+
+from protean import Aggregate, ValueObject
+from protean.core import field
+
+
+class Currency(Enum):
+    """ Set of choices for the status"""
+    USD = 'USD'
+    INR = 'INR'
+    CAD = 'CAD'
+
+
+class AccountType(Enum):
+    SAVINGS = 'SAVINGS'
+    CHECKING = 'CHECKING'
+
+
+@ValueObject(aggregate='account', bounded_context='customer')
+class Balance:
+    """A composite amount object, containing two parts:
+        * currency code - a three letter unique currency code
+        * amount - a float value
+    """
+
+    currency = field.String(
+        max_length=3,
+        choices=Currency)
+    amount = field.Float()
+
+    @classmethod
+    def build(cls, **values):
+        assert all(attr in values for attr in ['currency', 'amount'])
+
+        return cls(currency=values['currency'], amount=values['amount'])
+
+    def _clone_with_values(self, **kwargs):
+        # FIXME Find a way to do this generically and move method to `BaseValueObject`
+        currency = kwargs.pop('currency', None)
+        amount = kwargs.pop('amount', None)
+        return Balance(currency=currency or self.currency,
+                       amount=amount or self.amount)
+
+
+@Aggregate(aggregate='account', bounded_context='customer', root=True)
+class Account:
+    name = field.String(max_length=50)
+    account_type = field.String(
+        max_length=15,
+        choices=AccountType)
+    balance = field.ValueObject(Balance)
