c5ca5aee75f337d6dd008d3038d964a53ee815d5
Subhash Bhushan <subhash.bhushan@gmail.com>
2019-09-23 16:43:31 -0700

    Add support for Command Handlers
    * Add new Domain Object type called `CommandHandler`
    * Trigger Command handlers for registered Commands
    * Brokers will initiate notification, similar to Brokers

---
+++ src/protean/core/command_handler.py
@@ -0,0 +1,75 @@
+# Standard Library Imports
+from abc import abstractmethod
+
+# Protean
+from protean.domain import DomainObjects
+
+
+class _CommandHandlerMetaclass(type):
+    """
+    This base metaclass processes the class declaration and constructs a meta object that can
+    be used to introspect the CommandHandler class later. Specifically, it sets up a `meta_` attribute on
+    the CommandHandler to an instance of Meta, either the default of one that is defined in the
+    CommandHandler class.
+
+    `meta_` is setup with these attributes:
+        * `command`: The command that this command handler is associated with
+    """
+
+    def __new__(mcs, name, bases, attrs, **kwargs):
+        """Initialize CommandHandler MetaClass and load attributes"""
+
+        # Ensure initialization is only performed for subclasses of CommandHandler
+        # (excluding CommandHandler class itself).
+        parents = [b for b in bases if isinstance(b, _CommandHandlerMetaclass)]
+        if not parents:
+            return super().__new__(mcs, name, bases, attrs)
+
+        # Remove `abstract` in base classes if defined
+        for base in bases:
+            if hasattr(base, 'Meta') and hasattr(base.Meta, 'abstract'):
+                delattr(base.Meta, 'abstract')
+
+        new_class = super().__new__(mcs, name, bases, attrs, **kwargs)
+
+        # Gather `Meta` class/object if defined
+        attr_meta = attrs.pop('Meta', None)
+        meta = attr_meta or getattr(new_class, 'Meta', None)
+        setattr(new_class, 'meta_', CommandHandlerMeta(name, meta))
+
+        return new_class
+
+
+class CommandHandlerMeta:
+    """ Metadata info for the CommandHandler.
+
+    Options:
+    - ``command``: The command that this command handler is associated with
+    """
+
+    def __init__(self, entity_name, meta):
+        self.command_cls = getattr(meta, 'command_cls', None)
+        self.broker = getattr(meta, 'broker', None)
+
+
+class BaseCommandHandler(metaclass=_CommandHandlerMetaclass):
+    """Base Subsciber class that should implemented by all Domain CommandHandlers.
+
+    This is also a marker class that is referenced when command handlers are registered
+    with the domain
+    """
+    element_type = DomainObjects.COMMAND_HANDLER
+
+    def __new__(cls, *args, **kwargs):
+        if cls is BaseCommandHandler:
+            raise TypeError("BaseCommandHandler cannot be instantiated")
+        return super().__new__(cls)
+
+    def __init__(self, domain, command_cls):
+        self.domain = domain
+        self.command_cls = command_cls
+
+    @abstractmethod
+    def notify(self, command):
+        """Placeholder method for recieving notifications on command"""
+        pass
---
+++ tests/command_handler/elements.py
@@ -0,0 +1,38 @@
+# Protean
+from protean.core.aggregate import BaseAggregate
+from protean.core.command import BaseCommand
+from protean.core.command_handler import BaseCommandHandler
+from protean.core.field.basic import Integer, String
+
+
+class Person(BaseAggregate):
+    first_name = String(max_length=50, required=True)
+    last_name = String(max_length=50, required=True)
+    age = Integer(default=21)
+
+    @classmethod
+    def add_newcomer(cls, person_dict):
+        """Factory method to add a new Person to the system"""
+        newcomer = Person(
+            first_name=person_dict['first_name'],
+            last_name=person_dict['last_name'],
+            age=person_dict['age'],
+            )
+
+        return newcomer
+
+
+class AddPersonCommand(BaseCommand):
+    first_name = String(max_length=50, required=True)
+    last_name = String(max_length=50, required=True)
+    age = Integer(default=21)
+
+
+class AddNewPersonCommandHandler(BaseCommandHandler):
+    """CommandHandler that adds a new person into the system"""
+
+    class Meta:
+        command_cls = AddPersonCommand
+
+    def notify(self, command):
+        print("Received command: ", command)
