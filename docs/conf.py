ffe54495f4854dedccde590c44ecd1d7333ab77f
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-07-02 11:10:32 -0700

    Fix version to 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = "Protean"
 year = "2019"
 author = "Subhash Bhushan C"
 copyright = "{0}, {1}".format(year, author)
-version = release = "version = release = '0.5.1'"
+version = release = "0.5.1"
 
 pygments_style = "autumn"
 templates_path = ["."]
--- src/protean/__init__.py
+++ src/protean/__init__.py
@@ -1,6 +1,6 @@
 """Primary Module to define version and expose packages"""
 
-__version__ = "__version__ = '0.5.1'"
+__version__ = "0.5.1"
 
 # Local/Relative Imports
 from .domain import Domain
