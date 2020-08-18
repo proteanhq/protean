8bf76474b0e3a30858cf41389dfb505c2526b677
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-08-17 22:09:28 -0700

    Fix broken Dict test

--- tests/impl/model/sqlalchemy_model/postgresql/test_json_datatype.py
+++ tests/impl/model/sqlalchemy_model/postgresql/test_json_datatype.py
@@ -1,12 +1,11 @@
 # Standard Library Imports
 from datetime import datetime
-from typing import Dict
 
 # Protean
 import pytest
 
 from protean.core.aggregate import BaseAggregate
-from protean.core.field.basic import DateTime, String
+from protean.core.field.basic import Dict, DateTime, String
 from sqlalchemy import types as sa_types
 
 
