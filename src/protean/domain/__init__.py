991f9f996eda0c86c2f02a03521dfbcd61b80d37
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-08-26 19:38:12 -0700

    Move out registry into its own module/file

--- src/protean/domain/__init__.py
+++ src/protean/domain/__init__.py
@@ -6,11 +6,6 @@ import importlib
 import logging
 import sys
 
-from collections import defaultdict
-from dataclasses import dataclass, field
-from enum import Enum
-from typing import Any, Dict
-
 # Protean
 import marshmallow
 
@@ -21,6 +16,7 @@ from protean.core.exceptions import (
     ObjectNotFoundError,
 )
 from protean.globals import current_uow
+from protean.domain.registry import _DomainRegistry
 from protean.utils import fully_qualified_name, DomainObjects
 from werkzeug.datastructures import ImmutableDict
 
@@ -33,57 +29,6 @@ logger = logging.getLogger("protean.domain")
 
 # a singleton sentinel value for parameter defaults
 _sentinel = object()
-
-
-@dataclass
-class _DomainRegistry:
-    _elements: Dict[str, dict] = field(default_factory=dict)
-
-    @dataclass
-    class DomainRecord:
-        name: str
-        qualname: str
-        class_type: str
-        cls: Any
-
-    def __post_init__(self):
-        """Initialize placeholders for element types"""
-        for element_type in DomainObjects:
-            self._elements[element_type.value] = defaultdict(dict)
-
-    def register_element(self, element_cls):
-        if element_cls.element_type.name not in DomainObjects.__members__:
-            raise NotImplementedError
-
-        element_name = fully_qualified_name(element_cls)
-
-        element = self._elements[element_cls.element_type.value][element_name]
-        if element:
-            # raise ConfigurationError(f'Element {element_name} has already been registered')
-            logger.debug(f"Element {element_name} was already in the registry")
-        else:
-            element_record = _DomainRegistry.DomainRecord(
-                name=element_cls.__name__,
-                qualname=element_name,
-                class_type=element_cls.element_type.value,
-                cls=element_cls,
-            )
-
-            self._elements[element_cls.element_type.value][
-                element_name
-            ] = element_record
-
-            logger.debug(
-                f"Registered Element {element_name} with Domain as a {element_cls.element_type.value}"
-            )
-
-    def delist_element(self, element_cls):
-        if element_cls.element_type.name not in DomainObjects.__members__:
-            raise NotImplementedError
-
-        element_name = fully_qualified_name(element_cls)
-
-        self._elements[element_cls.element_type.value].pop(element_name, None)
 
 
 class Domain(_PackageBoundObject):
