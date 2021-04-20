96ccd8ac3d791fe3ac68f84fe664f81dd99373b9
Subhash Bhushan <subhash.bhushan@gmail.com>
2021-04-20 06:55:42 -0700

    More miscellaneous fixes

---
+++ tests/views/test_view_with_redis.py
@@ -0,0 +1,10 @@
+import pytest
+
+from protean.core.field.basic import Integer, String
+from protean.core.view import BaseView
+
+
+class Person(BaseView):
+    first_name = String(max_length=50, required=True)
+    last_name = String(max_length=50)
+    age = Integer(default=21)
