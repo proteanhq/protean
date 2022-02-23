57d4611bce855fb2b53b8e19070a11839515927a
Subhash Bhushan <subhash.bhushan@gmail.com>
2022-02-23 13:37:25 -0800

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ dev_requires = (
 
 setup(
     name="protean",
-    version="0.9.0",
+    version="0.9.1",
     license="BSD 3-Clause License",
     description="Protean Application Framework",
     long_description="%s\n%s"
--- src/protean/__init__.py
+++ src/protean/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 from .core.aggregate import BaseAggregate
 from .core.application_service import BaseApplicationService
