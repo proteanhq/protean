63dc07340ebbb53f8c1d1d72f7c118dd7f7b1cc1
Vatssss <vatsalsaxena1@gmail.com>
2025-04-29 15:35:58 +0530

    updated __main__.py

--- src/protean/__main__.py
+++ src/protean/__main__.py
@@ -9,7 +9,7 @@ Why does this file exist, and why __main__? For more info, read:
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
 
-from protean.cli import main
+from protean.cli.generate import app
 
 if __name__ == "__main__":
-    main()
+    app(prog_name="protean")
