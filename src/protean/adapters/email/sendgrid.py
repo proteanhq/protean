93e1e61757661179d83a725199df11d87125492b
Subhash Bhushan <494533+subhashb@users.noreply.github.com>
2026-07-04 16:35:49 -0700

    Type email/sendgrid: annotate provider methods with BaseEmail/bool

--- src/protean/adapters/email/sendgrid.py
+++ src/protean/adapters/email/sendgrid.py
@@ -1,12 +1,13 @@
 """Define the send-grid email provider"""
 
 import logging
+from typing import Any
 
 from python_http_client.exceptions import HTTPError
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, TemplateId
 
-from protean.core.email import BaseEmailProvider
+from protean.core.email import BaseEmail, BaseEmailProvider
 from protean.exceptions import SendError
 
 logger = logging.getLogger(__name__)
@@ -15,19 +16,19 @@ logger = logging.getLogger(__name__)
 class SendgridEmailProvider(BaseEmailProvider):
     """An email provider for sending emails using the Sendgrid Service"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.sg_client = SendGridAPIClient(self.conn_info["API_KEY"])
 
-    def send_email(self, message):
+    def send_email(self, email_message: BaseEmail) -> bool:
         """Send messages via the sendgrid api"""
 
         email = Mail(
-            from_email=message.from_email or self.conn_info["DEFAULT_FROM_EMAIL"],
-            to_emails=message.to,
+            from_email=email_message.from_email or self.conn_info["DEFAULT_FROM_EMAIL"],
+            to_emails=email_message.to,
         )
-        email.dynamic_template_data = message.data
-        email.template_id = TemplateId(message.template)
+        email.dynamic_template_data = email_message.data
+        email.template_id = TemplateId(email_message.template)
 
         try:
             response = self.sg_client.send(email)
